acfa0b68f66e5c214fcfdb670a31468b7bc55b38
Juan David Serrano <jd.serranom@uniandes.edu.co>
2021-03-12 13:38:15 -0500

    Lo que llevo 1

---
+++ main.js
@@ -0,0 +1,50 @@
+//let urlData =
+// "https://gist.githubusercontent.com/josejbocanegra/9a28c356416badb8f9173daf36d1460b/raw/5ea84b9d43ff494fcbf5c5186544a18b42812f09/restaurant.json";
+let urlData = "./data.json";
+const promesaData = new Promise((resolve, reject) => {
+    let req = new XMLHttpRequest();
+    req.open('GET', urlData);
+    req.onload = function () {
+        if (req.status === 200) {
+            resolve(JSON.parse(req.response));
+        } else {
+            reject("No se recibió el detalle");
+        }
+    };
+    req.send()
+});
+
+let selectedCategory = undefined;
+
+promesaData.then(categorias=>{
+    const navigationBar = document.getElementById("navigation-bar");
+    categorias.forEach((categoria,i)=>{
+        if(i===0){
+            selectedCategory = categoria.name;
+        }
+        let listItem = document.createElement("li");
+        listItem.setAttribute("class", "nav-item")
+        let link = document.createElement("a");
+        link.setAttribute("class", "nav-link")
+        // link.setAttribute("href", "#")
+        link.onclick = ()=>{
+
+        }
+        link.textContent = categoria.name;
+        listItem.appendChild(link);
+        navigationBar.appendChild(listItem);
+    });
+}).catch(err => console.log(err))
+
+
+function onSection(){
+    console.log("Hello")
+}
+
+
+// < li
+// className = "nav-item" >
+//     < a
+// class Name = "nav-link disabled"
+// href = "#" > Disabled < /a>
+// </li>
